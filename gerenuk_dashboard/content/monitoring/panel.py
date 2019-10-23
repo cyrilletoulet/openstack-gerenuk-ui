878e423a1a4709c722cb17b497b97360df101ace
Iheb Eladib <iheb.eladib@univ-lille.fr>
2019-10-23 21:23:10 +0100

    adding panel.py

--- gerenuk_dashboard/content/monitoring/panel.py
+++ gerenuk_dashboard/content/monitoring/panel.py
@@ -1,1 +1,33 @@
+# This file is part of Gerenuk.
+#
+# Gerenuk is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# Gerenuk is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Gerenuk. If not, see <https://www.gnu.org/licenses/>.
+#
+# Cyrille TOULET <cyrille.toulet@univ-lille.fr>
+# Iheb ELADIB <iheb.eladib@univ-lille.fr>
+#
+#Thu Oct 24 14:48:27 CEST 2019
+
+
+from django.utils.translation import ugettext_lazy as _
+import horizon
+
+
+class Monitoring(horizon.Panel):
+    """
+    The monitoring panel class
+    """
+    name = _("Monitoring")
+    slug = "monitoring"
+    permissions = ('openstack.services.compute',)
 
