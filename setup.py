2b1954096895a12928ab2788100049a13a0947e7
Cyrille TOULET <cyrille.toulet@univ-lille.fr>
2020-02-14 14:20:45 +0100

    Prepare new release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name = "gerenuk_dashboard",
-    version = "1.2",
+    version = "1.3",
     description = "The Gerenuk dashboard for OpenStack",
     url = "https://github.com/cyrilletoulet/openstack-gerenuk-ui",
 
@@ -16,7 +16,7 @@ setup(
 
     keywords = "monitoring cloud openstack gerenuk",
 
-    author = "Iheb ELADIB",
+    author = "Cyrille TOULET, Iheb ELADIB",
     author_email = "hpc@univ-lille.fr",
 
     package_data = {"": ["README", "*.html", "*.po", "*.mo"],
